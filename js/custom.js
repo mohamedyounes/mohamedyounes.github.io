ccf745b7f917d086431ba235b273790506afad8f
mohamedyounes <s-mohammed.abdelati@alexu.edu.eg>
2017-08-12 10:15:51 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+/*global $, alert, console*/
+
+$(function () {
+  'use strict';
+  // Trigger Nice Scroll Plugin
+  //$('html').niceScroll({
+    //cursorcolor: '#f7600e',
+    //cursorwidth: 10,
+    //cursorborderradius: '3px',
+    //cursorborder: '1px solid #f7600e'
+  //});
+  // Scroll To Features 
+  //$('.header .arrow i').click(function () {
+    //$('html, body').animate({
+      //scrollTop: $('.features').offset().top
+    //}, 1000);
+  //});
+  //$('.hire').click(function () {
+    //$('html, body').animate({
+      //scrollTop: $('.our-team').offset().top
+    //}, 1000);
+  //});
+  // Show Hidden Items From Work
+  //$('.show-more').click(function () {
+    //$('.our-work .hiddenx').fadeIn(1000);
+  //});
+});
